4e41144b1a3dd123e0c7314698c8244e6adfd7a5
mesganaja@gmail.com <mesganaja@gmail.com>
2024-01-26 13:44:22 -0800

    updated the footer contact list links

--- src/App.jsx
+++ src/App.jsx
@@ -121,7 +121,6 @@ function App() {
 
       <div className="footer-section">
         <Footer message="MESSAGE" />
-
       </div>
 
     </>
--- src/footer.jsx
+++ src/footer.jsx
@@ -1,17 +1,17 @@
 import './footer.css'
 import githubLogo from './assets/images/icon-github-white.svg'
 import linkedinLogo from './assets/images/icon-linkedin-white.svg'
-import frontendMentorLogo from './assets/images/icon-frontend-mentor-white.svg' 
+import frontendMentorLogo from './assets/images/icon-frontend-mentor-white.svg'
 import githubLogoHover from './assets/images/icon-github-hover.svg'
 import linkedinLogoHover from './assets/images/icon-linkedin-hover.svg'
 import frontendMentorLogoHover from './assets/images/icon-frontend-mentor-hover.svg'
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 
 
 function Footer(props) {
 
-    const { register, handleSubmit, formState: { errors }} = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = (data) => {
         console.log(data);
@@ -19,23 +19,23 @@ function Footer(props) {
     return (
 
         <div className="footer-main-container">
-            
+
             <div className="contact-form">
 
                 <div className="description">
-                <span>Contact</span>
-                    <p>I would love to hear about your project and how I could help. Please fill in the form, 
+                    <span>Contact</span>
+                    <p>I would love to hear about your project and how I could help. Please fill in the form,
                         and I’ll get back to you as soon as possible.
-                </p>
+                    </p>
                 </div>
 
                 <form id="contact-form" action="" onSubmit={handleSubmit(onSubmit)} noValidate>
-                    <input type="text" placeholder="NAME"/>
-                    <input id="emailInput" type="email" placeholder="EMAIL" {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} style={errors.email ? {borderBottom:'1px solid red'}: {}} />
-                    {errors.email && <span className="error-message">Sorry, invalid format here</span> }
-                    <textarea name="Message" id="messageArea" cols="20" rows="5" placeholder={props.message }></textarea>   
+                    <input type="text" placeholder="NAME" />
+                    <input id="emailInput" type="email" placeholder="EMAIL" {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} style={errors.email ? { borderBottom: '1px solid red' } : {}} />
+                    {errors.email && <span className="error-message">Sorry, invalid format here</span>}
+                    <textarea name="Message" id="messageArea" cols="20" rows="5" placeholder={props.message}></textarea>
 
-                    <button type="submit">SEND MESSAGE</button>       
+                    <button type="submit">SEND MESSAGE</button>
                 </form>
             </div>
 
@@ -46,15 +46,15 @@ function Footer(props) {
 
                 <div className="contact-list">
 
-                    <a href="#"><img className="logo-icon" src={githubLogo} alt="github logo" 
+                    <a href="https://github.com/mesganaG"><img className="logo-icon" src={githubLogo} alt="github logo"
                         onMouseOver={e => e.currentTarget.src = githubLogoHover}
                         onMouseOut={e => e.currentTarget.src = githubLogo}
                     /></a>
-                    <a href="#"><img className="logo-icon" src={linkedinLogo} alt="linkedin logo" 
+                    <a href="#https://www.linkedin.com/in/mesgana-geletu-303048261/"><img className="logo-icon" src={linkedinLogo} alt="linkedin logo"
                         onMouseOver={e => e.currentTarget.src = linkedinLogoHover}
                         onMouseOut={e => e.currentTarget.src = linkedinLogo}
                     /></a>
-                    <a href="#"><img className="logo-icon" src={frontendMentorLogo} alt="twitter logo" 
+                    <a href="https://www.frontendmentor.io/profile/mesganaG"><img className="logo-icon" src={frontendMentorLogo} alt="frontend mentor logo"
                         onMouseOver={e => e.currentTarget.src = frontendMentorLogoHover}
                         onMouseOut={e => e.currentTarget.src = frontendMentorLogo}
                     /></a>
